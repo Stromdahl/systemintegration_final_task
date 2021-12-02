08ac96eeafb9aaeec3e1f7dbf7cf39b846444afd
Mattias Strömdahl <Mattias.stromdahl@gmail.com>
2021-12-02 19:13:56 +0100

    First commit again x2

---
+++ VehicleHotSpotBackend.Web/Controllers/UserController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VehicleHotSpotBackend.Web.Models;
+
+namespace VehicleHotSpotBackend.Web.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserContext _context;
+
+        public UserController(UserContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UserItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserItem>>> GetUserItems()
+        {
+            return await _context.UserItems.ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("authenticate")]
+        public async Task<ActionResult<UserItem>> Login(string userName, string pwd)
+        {
+            HttpClient client = new HttpClient();
+            UserItem user = null;
+            string baseUrl = "https://kyhdev.hiqcloud.net/api/cds/v1.0/user/authenticate";
+
+            HttpResponseMessage response = await client.GetAsync(baseUrl + $"?userName={userName}&pwd={pwd}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                user = await response.Content.ReadAsAsync<UserItem>();
+                return user;
+            }
+
+            return NotFound();
+        }
+
+        // GET: api/UserItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserItem>> GetUserItem(string id)
+        {
+            var userItem = await _context.UserItems.FindAsync(id);
+
+            if (userItem == null)
+            {
+                return NotFound();
+            }
+
+            return userItem;
+        }
+
+        // PUT: api/UserItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserItem(string id, UserItem userItem)
+        {
+            if (id != userItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/UserItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<UserItem>> PostUserItem(UserItem userItem)
+        {
+            _context.UserItems.Add(userItem);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetUserItem", new { id = userItem.Id }, userItem);
+            return CreatedAtAction(nameof(GetUserItem), new { id = userItem.Id }, userItem);
+        }
+
+        // DELETE: api/UserItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserItem(long id)
+        {
+            var userItem = await _context.UserItems.FindAsync(id);
+            if (userItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.UserItems.Remove(userItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserItemExists(string id)
+        {
+            return _context.UserItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ VehicleHotSpotBackend.Web/Models/UserItem.cs
@@ -0,0 +1,10 @@
+﻿namespace VehicleHotSpotBackend.Web.Models
+{
+    public class UserItem
+    {
+        public string Id { get; set; }
+        public string accessToken { get; set; }
+
+        public string customerId { get; set; }
+    }
+}
